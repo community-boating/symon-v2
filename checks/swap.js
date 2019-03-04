5064419c7c17f6f00d834229004801489b67317c
Jonathan <coleji@bc.edu>
2019-03-04 18:21:26 -0500

    enhanced swap to alarm if no swap configured

--- checks/swap.js
+++ checks/swap.js
@@ -4,11 +4,12 @@ const exec = require('child_process').exec;
 module.exports = function(args) {
 	return new Promise(function(resolve, reject) {
 		const maxSwap = Number(args[0])
-		exec("free | grep Swap | awk '{ print $3 }'", function(error, stdout, stderr) {
-			console.log(stdout.trim())
-			var result = Number(stdout.trim());
-			if (result != null) {
-				if (result > maxSwap) reject([true, "Swap using " + result + "KB; threshold was " + maxSwap])
+		exec("free | grep Swap", function(error, stdout, stderr) {
+			var regex = /Swap:\s*(\d+)\s+(\d+)\s+(\d+)/
+			var result = regex.exec(stdout.trim())
+			if (result) {
+				if (Number(result[1]) == 0) reject([true, "Max swap is 0"])
+				else if (Number(result[2]) > maxSwap) reject([true, "Swap using " + result[2] + "KB; threshold was " + maxSwap])
 				else resolve()
 			} else reject([false, "Unable to determine swap use"])
 		})
