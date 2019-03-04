b4f6998c60e748f9b01a1bd1df07861e1e9e0da9
root <root@vanguard15.cbi.local>
2019-03-04 17:11:31 -0500

    mdadmCheck verified on v15

--- checks/mdadmCheck.js
+++ checks/mdadmCheck.js
@@ -1,11 +1,24 @@
 // args[0]: device to check
 const exec = require('child_process').exec;
 
+var okStatuses = {
+	"clean": true
+}
+
 module.exports = function(args) {
 	return new Promise(function(resolve, reject) {
 		const maxSwap = Number(args[0])
 		exec('mdadm --detail ' + args[0] + ' | grep "State :"', function(error, stdout, stderr) {
 			console.log(stdout.trim())
+			const regex = /State : (\S*)/
+			const result = regex.exec(stdout.trim())
+			if (result && result[1]) {
+				if (okStatuses[result[1]]) resolve();
+				else reject([true, "Found non-clean status '" + result[1] + "' from device " + args[0]])
+			} else {
+				reject([false, "Unable to get mdadm status for device " + args[0]])
+			}
+			console.log(result)
 			resolve()
 		})
 	});
