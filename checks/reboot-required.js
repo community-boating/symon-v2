207c87bb97c5de664ace43381e3134281285fd87
Jonathan <coleji@bc.edu>
2021-01-18 11:29:34 -0500

    reboot-required verified

--- checks/reboot-required.js
+++ checks/reboot-required.js
@@ -1,11 +1,9 @@
-//
-
 const exec = require('child_process').exec;
 
 module.exports = function(args) {
 	return new Promise(function(resolve, reject) {
-		exec("cat /var/run/reboot-required > /dev/null 2>&1; echo $?", function(error, stdout, stderr) {
-			if (stdout == "1") reject([true, "Reboot required"])
+		exec("sudo cat /var/run/reboot-required > /dev/null 2>&1; echo $?", function(error, stdout, stderr) {
+			if (String(stdout).trim() == "0") reject([true, "Reboot required"])
 			else resolve();
 		})
 	});
